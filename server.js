6aca3a7630221ca9afca2d96609aaed3ff50e6d8
Krish Patel <krishpatel18984@gmail.com>
2022-11-21 00:30:49 +0530

    Added Choice for admin

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true})); //not using this will give empty req.body
 
-setTimeout(paginatedRes, 500);
-
 //GET method to print and solve all students
 app.get("/all_students",async (req, res) => {
     try{
@@ -39,12 +37,10 @@ app.get("/all_students",async (req, res) => {
             }
         }
         results.results = allstd.rows.slice(startInd, endInd);
-        console.log(results);
+
         res.paginatedResults = results;
 
-        console.log(res.paginatedResults);
-
-        res.render('admin/all_studs.ejs', {data: res.paginatedResults});
+        res.status(200).render('admin/all_studs.ejs', {data: res.paginatedResults});
         //print
     }
     catch(err){
@@ -78,12 +74,10 @@ app.get("/all_employees",async (req, res) => {
             }
         }
         results.results = allstd.rows.slice(startInd, endInd);
-        console.log(results);
+        
         res.paginatedResults = results;
 
-        console.log(res.paginatedResults);
-
-        res.render('admin/all_employees.ejs', {data: res.paginatedResults});
+        res.status(200).render('admin/all_employees.ejs', {data: res.paginatedResults});
         //print
     }
     catch(err){
@@ -91,82 +85,69 @@ app.get("/all_employees",async (req, res) => {
     }
 });
 
+
+//home page redirect
 app.get('/', (req, res) => {
     res.status(301).redirect('/home')
 });
 
+//home page main link
 app.get('/home', (req,res) => {
     res.status(200).render('home/home.ejs')
 });
 
+//student sign in done by admin
 app.get('/s_signin_basic', (req,res) => {
     res.status(200).render('student/s_signin_basic.ejs')
 });
 
+//student sign in addrs by admin
 app.get('/s_signin_address', (req, res) => {
     res.status(200).render('student/s_signin_address.ejs')
 });
 
+//instructor sign in basic done by admin
 app.get('/i_signin_basic', (req,res) => {
     res.status(200).render('instructor/i_signin_basic.ejs')
 });
 
+//instrictor sign in addrs done by admin
 app.get('/i_signin_address', (req, res) => {
     res.status(200).render('instructor/i_signin_address.ejs')
 });
 
+//after log out home screen
 app.get('/login', (req, res) => {
     res.status(301).redirect('/home')
 });
 
+//instructor login page
 app.get('/i_login', (req, res) => {
     res.status(200).render('instructor/i_login.ejs')
 });
 
+//student log in page
 app.get('/s_login', (req, res) => {
     res.status(200).render('student/s_login.ejs')
 });
 
+//admin login page
 app.get('/a_login', (req,res)=>{
     res.status(200).render('admin/a_login.ejs')
 });
 
+//admn login page post method
 app.post('/a_login', (req,res) => {
     if(req.body.a_passwd == "admin"){
-        res.status(304).redirect('/all_students');
+        res.status(304).redirect('/a_choice');
     }else{
-        res.status(404).send("Wrong Pass")
+        res.status(404).send("Wrong Pass");
     }
 });
 
-function paginatedRes(model) {
-    return (req,res,next) => {
-        console.log(model);
-        const page = parseInt(req.query.page);
-        const limit = parseInt(req.query.limit);
-
-        const startInd = (page-1)*limit;
-        const endInd = (page)*limit;
-
-        const results = {};
-
-        if(endInd < model.length){
-            results.next = {
-                page: page+1,
-                limit: limit
-            }
-        }
-        if(startInd > 0){
-            results.prev = {
-                page: page-1,
-                limit:limit
-            }
-        }
-        results.results = model.slice(startInd, endInd);
-        console.log(results);
-        res.paginatedResults = results;
-        next();
-    }
-}
+//admin choices to show
+app.get('/a_choice', (req,res) => {
+    res.status(200).render('admin/choice.ejs');
+})
 
 app.listen(5000);
