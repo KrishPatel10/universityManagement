6af34d035a93022aac9a3c0bb24cca1b1aa20b11
Krish Patel <krishpatel18984@gmail.com>
2022-11-21 19:02:22 +0530

    added instructor login, student login

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const moment = require('moment')
 const app = express()
 const pool = require('./pggres_db')
 const i_log = require('./valids/student')
@@ -7,80 +8,80 @@ const bodyParser = require('body-parser');
 const { result } = require('@hapi/joi/lib/base');
 
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true})); //not using this will give empty req.body
+app.use(bodyParser.urlencoded({ extended: true })); //not using this will give empty req.body
 
 //GET method to print and solve all students
-app.get("/all_students",async (req, res) => {
-    try{
+app.get("/all_students", async (req, res) => {
+    try {
         const allstd = await pool.query('SELECT * FROM pms2.student_info');
 
         const page = parseInt(req.query.page);
         const limit = parseInt(req.query.limit);
 
-        const startInd = (page-1)*limit;
-        const endInd = (page)*limit;
+        const startInd = (page - 1) * limit;
+        const endInd = (page) * limit;
 
         const results = {};
 
-        if(endInd < allstd.rows.length){
+        if (endInd < allstd.rows.length) {
             results.next = {
-                page: page+1,
+                page: page + 1,
                 limit: limit
             }
         }
-        if(startInd > 0){
+        if (startInd > 0) {
             results.prev = {
-                page: page-1,
-                limit:limit
+                page: page - 1,
+                limit: limit
             }
         }
         results.results = allstd.rows.slice(startInd, endInd);
 
         res.paginatedResults = results;
 
-        res.status(200).render('admin/all_studs.ejs', {data: res.paginatedResults});
+        res.status(200).render('admin/all_studs.ejs', { data: res.paginatedResults });
         //print
     }
-    catch(err){
+    catch (err) {
         console.error(err.message);
     }
 });
 
 //GET method to print and solve all Employee
-app.get("/all_employees",async (req, res) => {
-    try{
+app.get("/all_employees", async (req, res) => {
+    try {
         const allstd = await pool.query('SELECT * FROM pms2.employee_info');
 
         const page = parseInt(req.query.page);//get value from url
         const limit = parseInt(req.query.limit);
 
-        const startInd = (page-1)*limit;
-        const endInd = (page)*limit;
+        const startInd = (page - 1) * limit;
+        const endInd = (page) * limit;
 
         const results = {};
 
-        if(endInd < allstd.rows.length){
+        if (endInd < allstd.rows.length) {
             results.next = {
-                page: page+1,
+                page: page + 1,
                 limit: limit
             }
         }
-        if(startInd > 0){
+        if (startInd > 0) {
             results.prev = {
-                page: page-1,
-                limit:limit
+                page: page - 1,
+                limit: limit
             }
         }
         results.results = allstd.rows.slice(startInd, endInd);
-        
+
         res.paginatedResults = results;
 
-        res.status(200).render('admin/all_employees.ejs', {data: res.paginatedResults});
+        res.status(200).render('admin/all_employees.ejs', { data: res.paginatedResults });
         //print
     }
-    catch(err){
+    catch (err) {
         console.error(err.message);
     }
 });
@@ -92,12 +93,12 @@ app.get('/', (req, res) => {
 });
 
 //home page main link
-app.get('/home', (req,res) => {
+app.get('/home', (req, res) => {
     res.status(200).render('home/home.ejs')
 });
 
 //student sign in done by admin
-app.get('/s_signin_basic', (req,res) => {
+app.get('/s_signin_basic', (req, res) => {
     res.status(200).render('student/s_signin_basic.ejs')
 });
 
@@ -107,7 +108,7 @@ app.get('/s_signin_address', (req, res) => {
 });
 
 //instructor sign in basic done by admin
-app.get('/i_signin_basic', (req,res) => {
+app.get('/i_signin_basic', (req, res) => {
     res.status(200).render('instructor/i_signin_basic.ejs')
 });
 
@@ -132,22 +133,80 @@ app.get('/s_login', (req, res) => {
 });
 
 //admin login page
-app.get('/a_login', (req,res)=>{
+app.get('/a_login', (req, res) => {
     res.status(200).render('admin/a_login.ejs')
 });
 
 //admn login page post method
-app.post('/a_login', (req,res) => {
-    if(req.body.a_passwd == "admin"){
+app.post('/a_login', (req, res) => {
+    console.log(req.body);
+    if (req.body.a_passwd == "admin") {
         res.status(304).redirect('/a_choice');
-    }else{
+    } else {
         res.status(404).send("Wrong Pass");
     }
 });
 
 //admin choices to show
-app.get('/a_choice', (req,res) => {
+app.get('/a_choice', (req, res) => {
     res.status(200).render('admin/choice.ejs');
-})
+});
+
+//instructor login
+app.post('/i_login', async (req, res) => {
+    try {
+        const allstd = await pool.query(`SELECT * FROM pms2.employee_info WHERE employee_id = ${req.body.i_id}`);
+
+        if (allstd.rows.length > 0) {
+            allstd.rows[0].joining_date = (allstd.rows[0].joining_date).toISOString().slice(0,10);
+            allstd.rows[0].date_of_birth = (allstd.rows[0].date_of_birth).toISOString().slice(0,10);
+            if (req.body.i_passwd == allstd.rows[0].employee_login_password) {
+                res.status(200).render('instructor/i_info.ejs', {data: allstd.rows[0]});
+            } else {
+                res.status(404).send("Wrong Pass");
+            }
+        } else {
+            res.status(404).send("User ID not found");
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//edit employee for employee
+app.get('/i_edit',async (req, res) => {
+    try {
+        const allstd = await pool.query(`SELECT * FROM pms2.employee_info WHERE employee_id = ${req.query.id}`);
+
+        if (allstd.rows.length > 0) {
+            allstd.rows[0].joining_date = (allstd.rows[0].joining_date).toISOString().slice(0,10);
+            allstd.rows[0].date_of_birth = (allstd.rows[0].date_of_birth).toISOString().slice(0,10);
+            res.status(200).render('instructor/i_edit.ejs', {data: allstd.rows[0]});
+        }else {
+            res.status(404).send("User ID not found");
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.post('/i_edit', async(req,res) => {
+    try {
+        console.log(req.body);
+        const qury = `UPDATE pms2.employee_info 
+        SET employee_email = $1, 
+        street = $2, 
+        city = $3, 
+        pincode=$4 
+        WHERE employee_id=$5`;
+        await pool.query(qury, [req.body.email, req.body.street, req.body.city, req.body.pincode, req.body.id]);
+        const allstd = await pool.query(`SELECT * FROM pms2.employee_info WHERE employee_id=${req.body.id}`);
+        allstd.rows[0].joining_date = (allstd.rows[0].joining_date).toISOString().slice(0,10);
+        allstd.rows[0].date_of_birth = (allstd.rows[0].date_of_birth).toISOString().slice(0,10);
+        res.status(200).render('instructor/i_info.ejs', {data: allstd.rows[0]});
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen(5000);
